0b4e6cd73308f39f9f144d8425a7b93e70fafd53
deme <dpipi17@freeuni.edu.ge>
2019-04-06 01:41:29 +0400

    finished brain, except Adversary

--- tetris/JBrainTetris.java
+++ tetris/JBrainTetris.java
@@ -11,6 +11,9 @@ import javax.swing.UIManager;
 public class JBrainTetris extends JTetris {
 	JCheckBox brainMode;
 	Brain br;
+	JCheckBox AnimateFall;
+	private int countTemp;
+	Brain.Move bestMove;
 	
 	
 	/**
@@ -30,21 +33,63 @@ public class JBrainTetris extends JTetris {
 	public JComponent createControlPanel() {
 		JPanel newPanel = (JPanel)super.createControlPanel();
 		newPanel.setLayout(new BoxLayout(newPanel, BoxLayout.Y_AXIS));
-
+		
 		newPanel.add(new JLabel("Brain:")); 
-		brainMode = new JCheckBox("Brain active"); 
+		brainMode = new JCheckBox("Brain active", false); 
 		newPanel.add(brainMode);
+		
+		AnimateFall = new JCheckBox("Animate Falling", true); 
+		newPanel.add(AnimateFall);
 
 		
 		return newPanel;
 	}
-
 	
+	@Override
+	public void tick(int verb) {
+		
+		if(verb == JTetris.DOWN && brainMode.isSelected()) {
+			
+			// new Piece situation
+			if(countTemp != count) {
+				countTemp = count;
+				// return board in committed situation
+				board.undo();
+				bestMove = br.bestMove(board, currentPiece, board.getHeight(), bestMove);
+			}
+			
+			if(bestMove != null) {
+				changes(bestMove);
+			}
+		}
+		
+		// call parent's tick function
+		super.tick(verb);
+	}
+	
+	
+	//  brain may do up to one rotation and one left/right move each time 
+	private void changes(Brain.Move bestMove) {
+		// if current Piece and best situation piece is not same, program should rotate current piece.
+		if(!currentPiece.equals(bestMove.piece)) {
+			super.tick(ROTATE);
+		}
+		
+		// piece needs to move right
+		if(bestMove.x > currentX) {
+			super.tick(RIGHT);
+		} else if(bestMove.x < currentX) { // piece needs to move left
+			super.tick(LEFT);
+		} else  if((bestMove.x == currentX && !AnimateFall.isSelected()) && currentY > bestMove.y) {
+			// if animateFall is not selected, program drops piece to its best position
+			super.tick(DROP);
+		}
+		
+	}
 	/**
 	 Creates a frame with a JBrainTetris.
 	*/
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception ignored) { }
