7afcd38c4085f7c2a9320199344050f5fd540a61
deme <dpipi17@freeuni.edu.ge>
2019-04-07 15:41:08 +0400

    maybe finished

--- tetris/JBrainTetris.java
+++ tetris/JBrainTetris.java
@@ -1,11 +1,14 @@
 package tetris;
 
+import java.awt.Dimension;
+
 import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.UIManager;
 
 public class JBrainTetris extends JTetris {
@@ -13,15 +16,21 @@ public class JBrainTetris extends JTetris {
 	Brain br;
 	JCheckBox AnimateFall;
 	private int countTemp;
-	Brain.Move bestMove;
 	
+	Brain.Move bestMove;
+	Brain.Move badMove;
+	
+	
+	JLabel ok;
+	JPanel little;
+	JSlider adversary;
 	
 	/**
 	 * Creates a new JBrainTetris;
 	 * 
 	 */
 	JBrainTetris(int pixels) {
-		//uses its parent constructor.
+		//uses its parent's constructor.
 		super(pixels);
 		
 		// creates  DefaultBrain object;
@@ -33,6 +42,19 @@ public class JBrainTetris extends JTetris {
 	public JComponent createControlPanel() {
 		JPanel newPanel = (JPanel)super.createControlPanel();
 		newPanel.setLayout(new BoxLayout(newPanel, BoxLayout.Y_AXIS));
+		
+		// make a little panel, put a JSlider in it. JSlider responds to getValue() 
+		little = new JPanel(); 
+		little.add(new JLabel("Adversary:")); 
+		adversary = new JSlider(0, 100, 0);    // min, max, current 
+		adversary.setPreferredSize(new Dimension(100,15)); 
+		little.add(adversary);
+		newPanel.add(little);
+		
+		JPanel okPan = new JPanel();
+		ok = new JLabel("ok");
+		okPan.add(ok);
+		newPanel.add(okPan);
 		
 		newPanel.add(new JLabel("Brain:")); 
 		brainMode = new JCheckBox("Brain active", false); 
@@ -67,6 +89,36 @@ public class JBrainTetris extends JTetris {
 		super.tick(verb);
 	}
 	
+	
+	@Override
+	public Piece pickNextPiece() {
+		Piece nextPiece = super.pickNextPiece();
+		int advVal = adversary.getValue();
+		
+		if(random.nextInt(99) >= advVal) {	
+			//update ok status
+			ok.setText("ok");
+			return nextPiece;
+		} else {
+			ok.setText("*ok*");
+			// current worst score
+			double largestScore = Integer.MIN_VALUE; 
+			
+			for(int i = 0; i < Piece.getPieces().length ; i++) {
+				Piece currPiece = Piece.getPieces()[i];
+				badMove = br.bestMove(board, currPiece, board.getHeight(), badMove);
+				if(badMove == null) return nextPiece;
+				
+				if(badMove.score > largestScore) {
+					nextPiece = currPiece;
+					largestScore = badMove.score;
+				}
+			}
+			
+			return nextPiece;
+			
+		}
+	} 
 	
 	//  brain may do up to one rotation and one left/right move each time 
 	private void changes(Brain.Move bestMove) {
